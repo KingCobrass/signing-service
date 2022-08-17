ff45c7044e7e9edc597bebe6f2abf1c466bfd751
KingCobrass <mahi.tshooter@gmail.com>
2022-08-18 01:05:20 +0600

    - Added environment configuration file - Logger file though needs sometime to make it workable - Set persistence DB in a location instead of in-memory with Truncate enabled

--- domain/device.go
+++ domain/device.go
@@ -7,3 +7,15 @@ type Device struct {
 	Algorithm string `json:"algorithm"`
 	Abc       string `json:"abc"`
 }
+
+type Direction int
+
+const (
+	ECC Direction = iota
+	RSA
+)
+
+type ISignature interface {
+	CreateSignatureDevice(id string, algorithm Direction, label string) error
+	SignTransaction(deviceId string, data string) ([]byte, error)
+}
